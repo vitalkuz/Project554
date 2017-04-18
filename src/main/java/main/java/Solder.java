5cd3acf7eb3066da0067a1c2d63db22ee2bdd922
Vsevolod Prokhortsev <seva.tver@ya.ru>
2017-04-18 18:17:34 +0300

    Update Solder.java

--- src/main/java/main/java/Solder.java
+++ src/main/java/main/java/Solder.java
@@ -3,7 +3,8 @@ package main.java;
 class Solder {
     private Integer x;
     private Integer y;
-    private Integer hp;
+    public Integer hp; //private
+    public final Integer fullHp; //private
     private Up7 field;
     private Integer team;
 
@@ -13,6 +14,8 @@ class Solder {
         this.x = x;
         this.y = y;
         this.team = team;
+        this.hp = 80 + (int)(Math.random()*40);
+        this.fullHp = this.hp;
         field.repaint();
     }
 
@@ -24,6 +27,14 @@ class Solder {
 
     int getTeam() {
         return team;
+    }
+
+    int getHp() {
+        return hp;
+    }
+
+    double percentOfHp() {
+        return (double)this.hp/this.fullHp;
     }
 
     int getX() {
